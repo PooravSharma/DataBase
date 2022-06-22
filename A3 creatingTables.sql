669bd1191595b99418312f05c211d0ba1ca7a6c4
Poorav Sharma <57639207+PooravSharma@users.noreply.github.com>
2022-06-23 07:49:31 +0800

    Add files via upload

--- A3 creatingTables.sql
+++ A3 creatingTables.sql
@@ -1,32 +1,37 @@
 
-CREATE TABLE Team (
-TeamName VARCHAR (50) PRIMARY KEY  NOT NULL,
-Founded INT NOT NULL, 
-Members INT NOT NULL, 
-Points_Scored INT NOT NULL, 
-Points_Conceded INT NOT NULL
-);
+CREATE TABLE [dbo].[Team]
+(
+[TeamName] VARCHAR (50) PRIMARY KEY  NOT NULL,
+[Founded] INT NOT NULL, 
+[Members] INT NOT NULL, 
+[Points_Scored] INT NOT NULL, 
+[Points_Conceded] INT NOT NULL
 
-CREATE TABLE Ladder(
-Position INT PRIMARY KEY NOT NULL,
-TeamName VARCHAR (50) NOT NULL,
-Played INT NOT NULL,
-Wins INT NOT NULL,
-Lost INT NOT NULL,
-Points INT NOT NULL,
-FOREIGN KEY (TeamName) REFERENCES Team(TeamName)
-);
+)
 
-CREATE TABLE Players (
-PlayerID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-PlayerName VARCHAR (50) NOT NULL,
-Attack INT,
-Scoring INT,
-Defending INT,
-Passing INT,
-TeamName VARCHAR (50) NOT NULL
-FOREIGN KEY (TeamName) REFERENCES Team(TeamName)
-);
+CREATE TABLE [dbo].[Ladder]
+(
+[Position] INT PRIMARY KEY NOT NULL,
+[TeamName] VARCHAR (50)  NOT NULL,
+[Played] INT NOT NULL,
+[Wins] INT NOT NULL,
+[Lost] INT NOT NULL,
+[Points] INT NOT NULL,
+CONSTRAINT FK_TeamName FOREIGN KEY ([TeamName]) REFERENCES Team([TeamName])
+	
+)
+
+CREATE TABLE [dbo].[Players]
+(
+[PlayerID] INT PRIMARY KEY NOT NULL,
+[PlayerName] VARCHAR (50) NOT NULL,
+[Attack] INT,
+[Scoring] INT,
+[Defending] INT,
+[Passing] INT,
+[TeamName] VARCHAR (50) NOT NULL
+CONSTRAINT FKTeamName FOREIGN KEY ([TeamName]) REFERENCES Team([TeamName])
+)
 
 
 
